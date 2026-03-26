d40a11453ddd0dbd3c57814d3d26aa1c50f2dd32
Mateusz Tadeusiak <mateusz.tadeusiak@atos.net>
2026-03-26 16:14:30 +0100

    refactor

--- src/main/java/pl/blillcalculator/bartek/gui/MenuItem.java
+++ src/main/java/pl/blillcalculator/bartek/gui/MenuItem.java
@@ -4,32 +4,36 @@ import java.util.Objects;
 
 public class MenuItem {
 
-    private String name;
-    private double price;
+  private String name;
+  private double price;
 
-    public MenuItem(String name, double price) {
-        this.name = name;
-        this.price = price;
-    }
+  public MenuItem(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public double getPrice() {
-        return price;
-    }
+  public double getPrice() {
+    return price;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MenuItem menuItem = (MenuItem) o;
-        return Objects.equals(name, menuItem.name);
-    }
+  @Override
+  public boolean equals(Object o) {
+      if (this == o) {
+          return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+          return false;
+      }
+    MenuItem menuItem = (MenuItem) o;
+    return Objects.equals(name, menuItem.name);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(name);
+  }
 }
